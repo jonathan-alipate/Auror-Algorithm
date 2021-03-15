6044fd4319252f94153487087d7ad44a799b0624
Jonathan Alipate <j.alipate@gmail.com>
2021-03-16 10:43:08 +1300

    MVP completed

--- interpolate.js
+++ interpolate.js
@@ -1,13 +1,13 @@
 function interpolate(string, dictionary) {
   const wordArray = string.split(" ");
   //loop through sentence and identify tokens
-  const interpolatedString = wordArray.map((word) => {
+  const interpolatedArray = wordArray.map((word) => {
     if (checkIfToken(word)) {
       //strip brackets
       const tokenWord = word.slice(1, word.length - 1);
-      //if word is still bounded by brackets after being stripped, it has double brackets
+      //if word is still bounded by brackets after being stripped, it has double brackets, if not it has single brackets.
       if(checkIfToken(tokenWord)){
-        console.log(tokenWord)
+        return tokenWord
       } else {
           //check dictionary for key word, if it exists in dictionary then return it. If not, return token.
           if(tokenWord in dictionary){
@@ -17,9 +17,12 @@ function interpolate(string, dictionary) {
           }
       }
     } else {
-      console.log(word)
+      return word
     }
   })
+  //take parsed array and concatenate it back into a single sentece.
+  const interpolatedString = interpolatedArray.join(' ')
+  console.log(interpolatedString)
 }
 
 //this function takes a single word and returns true if it is bounded by [] or [[]] and false if not.
