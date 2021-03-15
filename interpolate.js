eb9a22e91ded8acb08abdcf28db2ec0a52f14a0b
Jonathan Alipate <j.alipate@gmail.com>
2021-03-16 10:03:07 +1300

    created getTokens function for parsing sentence

--- interpolate.js
+++ interpolate.js
@@ -1,5 +1,24 @@
+//this function strips brackets from token
+
+//this function takes a given string and parses it to identify words bounded by [], it then returns identified tokens in an array.
+//if none exist, returns an empty array.
+function getTokens(string){
+    //break sentence into an array of words
+    const wordArray = string.split(" ")
+    const tokenArray = []
+    wordArray.forEach(word => {
+        //check if word is bounded by '[]' brackets(includes '[[]]')
+        if(word.charAt(0) === "[" && word.charAt(word.length - 1) === "]"){
+            tokenArray.push(word)
+        }
+    })
+    return tokenArray
+}
+
 function interpolate(string, dictionary){
 
 }
+
+getTokens("hi there, my name is johnny hello")
 
 module.exports = interpolate
