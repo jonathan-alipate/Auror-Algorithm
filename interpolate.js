b837e694867178e0289b50fe912de488e7a623a3
Jonathan Alipate <j.alipate@gmail.com>
2021-03-16 10:36:29 +1300

    implement checkIfToken function

--- interpolate.js
+++ interpolate.js
@@ -1,24 +1,36 @@
-//this function strips brackets from token
-
-//this function takes a given string and parses it to identify words bounded by [], it then returns identified tokens in an array.
-//if none exist, returns an empty array.
-function getTokens(string){
-    //break sentence into an array of words
-    const wordArray = string.split(" ")
-    const tokenArray = []
-    wordArray.forEach(word => {
-        //check if word is bounded by '[]' brackets(includes '[[]]')
-        if(word.charAt(0) === "[" && word.charAt(word.length - 1) === "]"){
-            tokenArray.push(word)
-        }
-    })
-    return tokenArray
+function interpolate(string, dictionary) {
+  const wordArray = string.split(" ");
+  //loop through sentence and identify tokens
+  const interpolatedString = wordArray.map((word) => {
+    if (checkIfToken(word)) {
+      //strip brackets
+      const tokenWord = word.slice(1, word.length - 1);
+      //if word is still bounded by brackets after being stripped, it has double brackets
+      if(checkIfToken(tokenWord)){
+        console.log(tokenWord)
+      } else {
+          //check dictionary for key word, if it exists in dictionary then return it. If not, return token.
+          if(tokenWord in dictionary){
+              return dictionary[tokenWord]
+          } else {
+              return tokenWord
+          }
+      }
+    } else {
+      console.log(word)
+    }
+  })
 }
 
-function interpolate(string, dictionary){
-
+//this function takes a single word and returns true if it is bounded by [] or [[]] and false if not.
+function checkIfToken(word) {
+  if (word.charAt(0) === "[" && word.charAt(word.length - 1) === "]") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-getTokens("hi there, my name is johnny hello")
+interpolate("Hello [name] [[author]]", { name: "Jim" });
 
-module.exports = interpolate
+module.exports = interpolate;
